4bb44e2233bc6c6a7a9d21bdf0f1d31b3694c75a
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-02-19 20:31:02 +0100

    Started creating a filter section on the homepage.

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -5,7 +5,38 @@ const filters = props => {
   return (
     <div className="filters-container">
       <div className="wrapper">
-        <span>filters</span>
+        <form>
+          <div className="search">
+            <input
+              type="text"
+              name="search"
+              value=""
+              placeholder="Que recherchez-vous?"
+            />
+            <button>Rechercher</button>
+          </div>
+          <div className="filters">
+            <div className="price-range">
+              <label htmlFor="price">Prix entre</label>
+              <select>
+                <option>Prix min</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+              </select>
+              et
+              <select>
+                <option>Prix max</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+              </select>
+            </div>
+            <select>
+              <option>Tri: Plus récents</option>
+            </select>
+          </div>
+        </form>
       </div>
     </div>
   );
