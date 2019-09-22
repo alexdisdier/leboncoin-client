50a4f9826707190d142e61b5287ca3002150523a
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-09-22 14:11:12 +0200

    corrected wrong path

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import App from "./app";
+import App from "./App";
 
 jest.mock("history/createBrowserHistory", () => () => "history");
 jest.mock("react-router-dom", () => ({
