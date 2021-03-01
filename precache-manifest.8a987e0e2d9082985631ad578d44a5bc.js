1f91f898752657aa1cc29190ffe0ae8130632503
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2021-03-01 22:07:39 +0100

    Updates

---
+++ precache-manifest.8a987e0e2d9082985631ad578d44a5bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccda09eb368492274f99",
+    "url": "/leboncoin-client/static/css/main.73a17bcb.chunk.css"
+  },
+  {
+    "revision": "ccda09eb368492274f99",
+    "url": "/leboncoin-client/static/js/main.ccda09eb.chunk.js"
+  },
+  {
+    "revision": "68bfb27db2ce6ad960a9",
+    "url": "/leboncoin-client/static/js/runtime~main.68bfb27d.js"
+  },
+  {
+    "revision": "aeda93d09fa876d64cac",
+    "url": "/leboncoin-client/static/css/2.e96e3654.chunk.css"
+  },
+  {
+    "revision": "aeda93d09fa876d64cac",
+    "url": "/leboncoin-client/static/js/2.aeda93d0.chunk.js"
+  },
+  {
+    "revision": "14ca361262f935be9c9adc735422d991",
+    "url": "/leboncoin-client/static/media/loading.14ca3612.svg"
+  },
+  {
+    "revision": "8a5ec7261b0475f791e46b42f3c9c081",
+    "url": "/leboncoin-client/static/media/logo.8a5ec726.svg"
+  },
+  {
+    "revision": "5af2cec4b3546eeda71129b7195c37cf",
+    "url": "/leboncoin-client/static/media/menu-button.5af2cec4.svg"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/leboncoin-client/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "8676d80353f9c4b692fcbb8b2016a0d6",
+    "url": "/leboncoin-client/static/media/chevronLeft.8676d803.svg"
+  },
+  {
+    "revision": "0b3171eceb9832f75de60343fda832bb",
+    "url": "/leboncoin-client/static/media/chevronRight.0b3171ec.svg"
+  },
+  {
+    "revision": "b4189ca0fbdb2df43eaede406ef78539",
+    "url": "/leboncoin-client/static/media/clock.b4189ca0.svg"
+  },
+  {
+    "revision": "13fb8d48f0d067f9bc7d7e8ecaf3cf8d",
+    "url": "/leboncoin-client/static/media/bell.13fb8d48.svg"
+  },
+  {
+    "revision": "63f6b158252db17abd019f10e0f7d1d4",
+    "url": "/leboncoin-client/static/media/eye.63f6b158.svg"
+  },
+  {
+    "revision": "567affe213a95252b32bae1bb6344985",
+    "url": "/leboncoin-client/index.html"
+  }
+];
